b1a161e185014df82b662d58771070c0fc856406
Samsoucy <newf930@outlook.com>
2019-02-28 17:07:17 -0500

    created migration for dishes

---
+++ migrations/20190228163222_dishes.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("dishes", function (tbl) {
+        tbl.increments();
+
+        tbl
+            .text("name")
+            .notNullable()
+            
+        tbl.timestamps(true, true);
+        
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dishes');
+};

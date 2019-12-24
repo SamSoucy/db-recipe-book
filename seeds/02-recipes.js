a0306e06a1aaa2eb2254ff0e8c6b90c94c6ced18
Samsoucy <newf930@outlook.com>
2019-12-24 14:05:37 -0500

    made small modification

--- seeds/02-recipes.js
+++ seeds/02-recipes.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('recipes').insert([
         {name: 'pepperoni pizza', dish_id: 1},
         {name: "vegie berger", dish_id: 2},
-        {name: "vegie fajitas", dish_id: 3}
+        { name: "vegie fajitas", dish_id: 3 }
       ]);
     });
 };
